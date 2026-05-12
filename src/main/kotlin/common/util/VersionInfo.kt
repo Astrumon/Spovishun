c6856b41afaf1b54401d5a6014e20da711e466e4
Danylo Bidnyk <dnlbdnk@gmail.com>
2026-05-12 21:00:31 +0300

    chore: bump version to 1.5.0

--- src/main/kotlin/common/util/VersionInfo.kt
+++ src/main/kotlin/common/util/VersionInfo.kt
@@ -1,7 +1,7 @@
 package com.ua.astrumon.common.util
 
 object VersionInfo {
-    const val VERSION = "1.4.0"
+    const val VERSION = "1.5.0"
     const val BOT_NAME = "Spovishun"
     fun getFullVersion(): String = BOT_NAME + " v" + VERSION
 }
